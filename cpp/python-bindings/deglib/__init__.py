c4890aa91780ae6350a82ad3f671de28f5c0d0b4
Bluemi <bruno.schilling@protonmail.ch>
2024-05-30 14:59:47 +0200

    ported load_readonly_graph() to python

--- cpp/python-bindings/deglib/__init__.py
+++ cpp/python-bindings/deglib/__init__.py
@@ -1,17 +1,6 @@
-import numpy as np
-from pathlib import Path
-
-from deglib_cpp import avx_usable, sse_usable, distance_float_l2
-
-
-def ivecs_read(fname: str | Path) -> np.ndarray:
-    a = np.fromfile(fname, dtype='int32')
-    d = a[0]
-    return a.reshape(-1, d + 1)[:, 1:].copy()
+from deglib_cpp import avx_usable, sse_usable
+from . import datasets
+from . import graph
 
 
-def fvecs_read(fname: str | Path) -> np.ndarray:
-    return ivecs_read(fname).view('float32')
-
-
-__all__ = ['avx_usable', 'sse_usable', 'distance_float_l2', 'ivecs_read', 'fvecs_read']
+__all__ = ['avx_usable', 'sse_usable', 'datasets', 'graph']
--- cpp/python-bindings/tests/deglib_anns_bench.py
+++ cpp/python-bindings/tests/deglib_anns_bench.py
@@ -1,7 +1,8 @@
 """
 Mirror of the c++ benchmark "cpp/benchmark/src/deglib_anns_bench.cpp".
 """
-
+import sys
+import pathlib
 
 import deglib
 
@@ -16,10 +17,24 @@ def test_deglib_anns_bench():
     else:
         print("use arch  ...")
 
-    arg1 = b'arg1'
-    arg2 = b'arg2'
-    a = deglib.distance_float_l2(arg1, arg2)
-    print('a:', a)
+    data_path: pathlib.Path = pathlib.Path(sys.argv[1])
+
+    query_file: pathlib.Path = data_path / "SIFT1M" / "sift_query.fvecs"
+    graph_file: pathlib.Path = (data_path / "deg" / "best_distortion_decisions" /
+                                "128D_L2_K30_AddK60Eps0.2High_SwapK30-0StepEps0.001LowPath5Rnd0"
+                                "+0_improveEvery2ndNonPerfectEdge.deg")
+    gt_file: pathlib.Path = data_path / "SIFT1M" / "sift_groundtruth.ivecs"
+
+    assert query_file.is_file(), 'Could not find query file: {}'.format(query_file)
+    assert graph_file.is_file(), 'Could not find graph file: {}'.format(graph_file)
+    assert gt_file.is_file(), 'Could not find ground truth file: {}'.format(gt_file)
+
+    graph = deglib.graph.load_readonly_graph(graph_file)
+
+    print("Graph with {} vertices".format(graph.size()))
+
+    query_repository = deglib::load_static_repository(query_file.c_str());
+    fmt::print("{} Query Features with {} dimensions \n", query_repository.size(), query_repository.dims());
 
 
 if __name__ == '__main__':
