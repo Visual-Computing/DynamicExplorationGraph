c7c79b62ca1029288858e35e04f521e4cb585611
Bluemi <bruno.schilling@protonmail.ch>
2024-06-19 12:16:05 +0200

    WIP: Complete python interface
    - add SearchGraph interface
    - add SpaceInterface
    - add some FloatSpace functions
    - add default arguments for EvenRegularGraphBuilder.__init__()

--- cpp/python-bindings/deglib/builder.py
+++ cpp/python-bindings/deglib/builder.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import numpy as np
 import deglib_cpp
 from .std import Mt19937
@@ -7,9 +9,14 @@ from .graph import SizeBoundedGraph
 class EvenRegularGraphBuilder:
     # TODO: add MutableGraph Interface
     def __init__(
-            self, graph: SizeBoundedGraph, rng: Mt19937, extend_k: int, extend_eps: float, improve_k: int,
-            improve_eps: float, max_path_length: int = 10, swap_tries: int = 3, additional_swap_tries: int = 3
+            self, graph: SizeBoundedGraph, rng: Mt19937, extend_k: Optional[int] = None, extend_eps: float = 0.2,
+            improve_k: Optional[int] = None, improve_eps: float = 0.001, max_path_length: int = 10, swap_tries: int = 3,
+            additional_swap_tries: int = 3
     ):
+        if improve_k is None:
+            improve_k = graph.get_edges_per_vertex()
+        if extend_k is None:
+            extend_k = graph.get_edges_per_vertex()
         self.builder_cpp = deglib_cpp.EvenRegularGraphBuilder(
             graph.to_cpp(), rng.to_cpp(), extend_k, extend_eps, improve_k, improve_eps, max_path_length, swap_tries,
             additional_swap_tries
