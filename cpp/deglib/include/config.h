fbb8eccc3279f33f3dc888d8ac08f31f0f78890d
Nico Hezel <Neiko-Armitage@gmx.de>
2024-08-04 21:35:27 +0200

    enable AVX and SSE even though better flags like AVX512 are set

--- cpp/deglib/include/config.h
+++ cpp/deglib/include/config.h
@@ -4,7 +4,7 @@
   // Microsoft Visual C++ does not define __SSE__ or __SSE2__ but _M_IX86_FP instead
   // https://docs.microsoft.com/en-us/cpp/preprocessor/predefined-macros?view=msvc-170
   #ifdef _MSC_VER
-    #if (defined(_M_AMD64) || defined(_M_X64) || defined(_M_IX86_FP) == 2)
+    #if defined(_M_AMD64) || defined(_M_X64) || defined(_M_IX86_FP) == 2
       #define __SSE__
       #define __SSE2__
     #elif defined(_M_IX86_FP) == 1
@@ -14,21 +14,24 @@
 
   #if defined(__AVX512F__)
     #define USE_AVX512
+  #endif
 
-  #elif defined(__AVX__) || defined(__AVX2__)
+  #if defined(__AVX__) || defined(__AVX2__)
     #define USE_AVX
+  #endif
 
-  #elif defined(__SSE__) || defined(__SSE2__)
+  #if defined(__SSE__) || defined(__SSE2__)
     #define USE_SSE
+  #endif
 
-  #else
+  #if !defined(USE_AVX) && !defined(USE_SSE) && !defined(USE_AVX512)
     #ifdef _MSC_VER
       #pragma message ( "warning: neither SSE, AVX nor AVX512 are defined" )
     #else
       #warning "neither SSE, AVX nor AVX512 are defined"
     #endif
-
   #endif
+
   // #undef USE_AVX512  // for testing arm processors
   // #undef USE_AVX
   // #undef USE_SSE
@@ -36,7 +39,7 @@
 
 // TODO switch to only #include <immintrin.h>
 // https://stackoverflow.com/questions/11228855/header-files-for-x86-simd-intrinsics
-#if defined(USE_AVX) || defined(USE_SSE)
+#if defined(USE_AVX) || defined(USE_SSE) || defined(USE_AVX512)
   #ifdef _MSC_VER
     #include <intrin.h>
     #include <stdexcept>
