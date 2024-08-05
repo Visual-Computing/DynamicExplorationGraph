82f02770e0bbe00a75e82714ac5ae04e7157e7af
Nico Hezel <Neiko-Armitage@gmx.de>
2024-08-05 11:30:59 +0200

    first test visit visited list

--- cpp/deglib/include/graph/readonly_graph.h
+++ cpp/deglib/include/graph/readonly_graph.h
@@ -10,6 +10,7 @@
 #include "deglib.h"
 #include "distances.h"
 #include "search.h"
+#include "visited_list_pool.h"
 
 namespace deglib::graph
 {
@@ -320,6 +321,8 @@ class ReadOnlyGraph : public deglib::search::SearchGraph {
   // distance calculation function between feature vectors of two graph vertices
   const deglib::FloatSpace feature_space_;
 
+  std::unique_ptr<VisitedListPool> visited_list_pool_;
+
 public:
   ReadOnlyGraph(const uint32_t max_vertex_count, const uint8_t edges_per_vertex, const deglib::FloatSpace feature_space)
       : max_vertex_count_(max_vertex_count),
@@ -338,6 +341,7 @@ public:
         feature_space_(feature_space) { 
 
     label_to_index_.reserve(max_vertex_count);
+    visited_list_pool_ = std::make_unique<VisitedListPool>(1, max_vertex_count);
   }
 
   /**
@@ -587,7 +591,9 @@ public:
     uint32_t distance_computation_count = 0;
 
     // set of checked vertex ids
-    auto checked_ids = std::vector<bool>(this->size());
+    VisitedList *vl = visited_list_pool_->getFreeVisitedList();
+    vl_type *checked_ids = vl->mass;
+    vl_type checked_ids_tag = vl->curV;
 
     // items to traverse next
     auto next_vertices = deglib::search::UncheckedSet();
@@ -600,8 +606,8 @@ public:
 
     // copy the initial entry vertices and their distances to the query into the three containers
     for (auto&& index : entry_vertex_indices) {
-      if(checked_ids[index] == false) {
-        checked_ids[index] = true;
+      if(checked_ids[index] != checked_ids_tag) {
+        checked_ids[index] = checked_ids_tag;
 
         const auto feature = reinterpret_cast<const float*>(this->feature_by_index(index));
         const auto distance = COMPARATOR::compare(query, feature, dist_func_param);
@@ -610,8 +616,10 @@ public:
 
         // early stop after to many computations
         if constexpr (use_max_distance_count) {
-          if(++distance_computation_count >= max_distance_computation_count)
+          if(++distance_computation_count >= max_distance_computation_count) {
+            visited_list_pool_->releaseVisitedList(vl);
             return results;
+          }
         }
       }
     }
@@ -634,10 +642,11 @@ public:
 
       size_t good_neighbor_count = 0;
       const auto neighbor_indices = this->neighbors_by_index(next_vertex.getInternalIndex());
+      memory::prefetch(reinterpret_cast<const char*>(checked_ids[neighbor_indices[0]]));
       for (size_t i = 0; i < this->edges_per_vertex_; i++) {
         const auto neighbor_index = neighbor_indices[i];
-        if (checked_ids[neighbor_index] == false)  {
-          checked_ids[neighbor_index] = true;
+        if (checked_ids[neighbor_index] != checked_ids_tag)  {
+          checked_ids[neighbor_index] = checked_ids_tag;
           good_neighbors[good_neighbor_count++] = neighbor_index;
         }
       }
@@ -672,12 +681,15 @@ public:
 
         // early stop after to many computations
         if constexpr (use_max_distance_count) {
-          if(++distance_computation_count >= max_distance_computation_count)
+          if(++distance_computation_count >= max_distance_computation_count) {
+            visited_list_pool_->releaseVisitedList(vl);
             return results;
+          }
         }
       }
     }
 
+    visited_list_pool_->releaseVisitedList(vl);
     return results;
   }
 
