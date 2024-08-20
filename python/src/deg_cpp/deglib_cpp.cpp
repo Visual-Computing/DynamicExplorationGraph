9995685c0271826abea25cca87a54e39cd12d897
Bluemi <bruno.schilling@protonmail.ch>
2024-08-20 12:50:14 +0200

    try fixing ssize_t for windows

--- python/src/deg_cpp/deglib_cpp.cpp
+++ python/src/deg_cpp/deglib_cpp.cpp
@@ -215,7 +215,7 @@ PYBIND11_MODULE(deglib_cpp, m) {
            [](const deglib::graph::ReadOnlyGraph &g, const uint32_t internal_idx) {
              const bool uint8_metric = g.getFeatureSpace().metric() == deglib::Metric::L2_Uint8;
              const char* format_descriptor = uint8_metric ? "B" : "f";
-             const ssize_t item_size = uint8_metric ? sizeof(uint8_t) : sizeof(float);
+             const size_t item_size = uint8_metric ? sizeof(uint8_t) : sizeof(float);
              return py::memoryview::from_buffer(
                  g.getFeatureVector(internal_idx),
                  item_size, format_descriptor, {g.getFeatureSpace().dim()}, {item_size});
@@ -256,7 +256,7 @@ PYBIND11_MODULE(deglib_cpp, m) {
          [](const deglib::graph::SizeBoundedGraph &g, const uint32_t internal_idx) {
            const bool uint8_metric = g.getFeatureSpace().metric() == deglib::Metric::L2_Uint8;
            const char* format_descriptor = uint8_metric ? "B" : "f";
-           const ssize_t item_size = uint8_metric ? sizeof(uint8_t) : sizeof(float);
+           const size_t item_size = uint8_metric ? sizeof(uint8_t) : sizeof(float);
            return py::memoryview::from_buffer(
                g.getFeatureVector(internal_idx),
                item_size, format_descriptor, {g.getFeatureSpace().dim()}, {item_size});
