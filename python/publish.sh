644f79873b68fb5151d85885382c8fece6e8a7c8
Bluemi <bruno.schilling@protonmail.ch>
2024-12-19 15:49:54 +0100

    v0.1.1

---
+++ python/publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# get new version from __init__.py
+new_version=$(grep --color=never -oP '__version__\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' "src/deglib/__init__.py")
+echo "detected new version: $new_version"
+
+# get old version from pypi
+old_version=$(curl -s "https://pypi.org/pypi/deglib/json" | jq -r '.info.version')
+
+# compare versions
+if [ "$old_version" = "$new_version" ]; then
+	echo "ERROR: current version equals version on pypi"
+	read -p "have you changed the version in \"src/deglib/__init__.py\"? y/[n]: " answer
+	if [ "$answer" != "y" ]; then
+		exit 0
+	fi
+fi
+
+# publish
+git add -A && git commit -m "v$new_version" && git tag -a "v$new_version" -m "v$new_version"
+git push && git push origin --tags
